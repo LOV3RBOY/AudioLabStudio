e131ca38cb7fb2c4dec652be56fc7e98b7a5bd62
LOV3RBOY <131902652+LOV3RBOY@users.noreply.github.com>
2025-06-14 01:51:30 -0700

    Delete packaged-app directory

--- packaged-app/setup.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Setting up AudioLabStudio...
-npm install --production
-echo Setup complete! Run start.bat to launch.
-pause
--- packaged-app/start.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Starting AudioLabStudio...
-node start.js
-pause
